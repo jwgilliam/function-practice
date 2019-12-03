092b5faedec4b9d838f15083b27624ec2a74143e
jwgilliam <ggsmoustache@gmail.com>
2019-12-03 09:14:17 -0600

    Function practice

---
+++ bestinshow/main.js
@@ -0,0 +1,11 @@
+const bestInShow = (dogBreed) => {
+  console.log("my favorite dog breed is " + dogBreed)
+  if (dogBreed = "meow") {
+    console.log("I like cats")
+  }
+}
+
+
+bestInShow("Shiba")
+
+const myFavorite = bestInShow
---
+++ practice.js
@@ -0,0 +1,9 @@
+const calculateAgeInDogYears = (ageOfPerson) => {
+  const ageInDogYears = ageOfPerson / 7
+  return ageInDogYears
+}
+
+calculateAgeInDogYears(22)
+
+console.log(dogAge)
+
